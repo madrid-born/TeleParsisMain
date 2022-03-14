d66d9f65a7a6bc86d9b883155de06d80649b756c
madrid-born <alirezanoori.1381@gmail.com>
2022-03-14 22:20:20 +0330

    ddwdw

--- bot.py
+++ bot.py
@@ -101,8 +101,7 @@ def adder(user_id):
         except:
             i = -1
     df2 = pd.DataFrame({'0': [user_id], '1': [0]})
-    file = pd.concat([file, df2], ignore_index=True, axis=0)
-    file = file[[0,1]]
+    file .append(df2, ignore_index=True)
     db_replace(file)
 
 
